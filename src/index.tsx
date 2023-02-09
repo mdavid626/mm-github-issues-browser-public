8d906ae300bdd76dd468f5de47348051207e16bb
Dávid Molnár <mdavid626@gmail.com>
2023-02-09 15:56:08 +0100

    Add basic apollo query client

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 /* istanbul ignore file */
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
@@ -6,15 +7,25 @@ import ErrorBoundary from './components/error-boundary/error-boundary';
 import './index.css';
 import Routes from './router/routes';
 
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Bearer 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`,
+  },
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <HashRouter>
-        <Routes />
-      </HashRouter>
+      <ApolloProvider client={client}>
+        <HashRouter>
+          <Routes />
+        </HashRouter>
+      </ApolloProvider>
     </ErrorBoundary>
   </React.StrictMode>
 );
--- src/pages/issues-page/issues-page.tsx
+++ src/pages/issues-page/issues-page.tsx
@@ -1,13 +1,37 @@
+import { useQuery, gql } from '@apollo/client';
 import React from 'react';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
 import './issues-page.css';
 
+const GET_LOCATIONS = gql`
+  query {
+    repository(owner: "octocat", name: "Hello-World") {
+      issues(last: 20, states: CLOSED) {
+        edges {
+          node {
+            title
+            url
+            labels(first: 5) {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 const IssuesPage = () => {
+  const { data } = useQuery(GET_LOCATIONS);
   return (
     <div className="IssuesPage">
       <Header />
-      <div className="IssuesPage-content">content</div>
+      <div className="IssuesPage-content">{JSON.stringify(data)}</div>
       <Footer />
     </div>
   );
