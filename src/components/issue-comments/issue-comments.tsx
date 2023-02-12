4d78ef49ffb8a69b89f6d7b9e6cc3a0c62a447e5
Dávid Molnár <mdavid626@gmail.com>
2023-02-12 09:15:05 +0100

    Adjust mobile styles

---
+++ src/components/issue-comments/issue-comments.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useFetchMoreIssueComment } from '../../hooks/issue-hooks/issue-hooks';
+import { IssueQueryItem } from '../../types/issue';
+import IssueCommentItem from '../issue-comment-item/issue-comment-item';
+import './issue-comments.css';
+
+const IssueComments: React.FC<{
+  issueNumber: number;
+  comments: IssueQueryItem['comments'];
+}> = ({ issueNumber, comments }) => {
+  const [fetchMoreComments, isFetchingMoreComments] =
+    useFetchMoreIssueComment(issueNumber);
+  return (
+    <div className="IssueComments">
+      <div className="IssueComments-title">
+        Comments ({comments.totalCount})
+      </div>
+      {comments.nodes.length === 0 ? (
+        <div>No comments yet</div>
+      ) : (
+        <div className="IssueComments-comments">
+          {comments.nodes.map((comment) => (
+            <IssueCommentItem key={comment.id} comment={comment} />
+          ))}
+        </div>
+      )}
+      {comments.pageInfo.hasNextPage && !isFetchingMoreComments && (
+        <div
+          onClick={() => fetchMoreComments(comments.pageInfo.endCursor)}
+          className="IssueComments-loadMore"
+        >
+          load more comments
+        </div>
+      )}
+      {isFetchingMoreComments && (
+        <div className="IssueComments-loading">Loading...</div>
+      )}
+    </div>
+  );
+};
+
+export default IssueComments;
--- src/components/issue-item/issue-item.tsx
+++ src/components/issue-item/issue-item.tsx
@@ -1,56 +1,28 @@
 import { parseISO, format } from 'date-fns';
 import React from 'react';
-import { useFetchMoreIssueComment } from '../../hooks/issue-hooks/issue-hooks';
 import { IssueQueryItem } from '../../types/issue';
-import IssueCommentItem from '../issue-comment-item/issue-comment-item';
+import IssueComments from '../issue-comments/issue-comments';
 import IssueStateBadge from '../issue-state-badge/issue-state-badge';
 import './issue-item.css';
 
 const IssueItem: React.FC<{
   issue: IssueQueryItem;
-}> = ({ issue }) => {
-  const [fetchMoreComments, isFetchingMoreComments] = useFetchMoreIssueComment(
-    issue.number
-  );
-  return (
-    <div className="IssueItem">
-      <div className="IssueItem-header">
-        <div className="IssueItem-titleAndState">
-          <div className="IssueItem-title">{issue.title}</div>
-          <IssueStateBadge state={issue.state} />
-        </div>
-        <div className="IssueItem-detail">
-          <div>#{issue.number}</div>
-          <div>{format(parseISO(issue.createdAt), 'dd.MM.yyyy HH:mm')}</div>
-          <div>{issue.author.login}</div>
-        </div>
+}> = ({ issue }) => (
+  <div className="IssueItem">
+    <div className="IssueItem-header">
+      <div className="IssueItem-titleAndState">
+        <div className="IssueItem-title">{issue.title}</div>
+        <IssueStateBadge state={issue.state} />
       </div>
-      <div className="IssueItem-body">{issue.body}</div>
-      <div className="IssueItem-commentsTitle">
-        Comments ({issue.comments.totalCount})
+      <div className="IssueItem-detail">
+        <div>#{issue.number}</div>
+        <div>{format(parseISO(issue.createdAt), 'dd.MM.yyyy HH:mm')}</div>
+        <div>{issue.author.login}</div>
       </div>
-      <div className="IssueItem-comments">
-        {issue.comments.nodes.map((comment) => (
-          <IssueCommentItem
-            key={comment.id}
-            comment={comment}
-            className="IssueItem-comment"
-          />
-        ))}
-      </div>
-      {issue.comments.pageInfo.hasNextPage && !isFetchingMoreComments && (
-        <div
-          onClick={() => fetchMoreComments(issue.comments.pageInfo.endCursor)}
-          className="IssueItem-loadMore"
-        >
-          load more comments
-        </div>
-      )}
-      {isFetchingMoreComments && (
-        <div className="IssueItem-loading">Loading...</div>
-      )}
     </div>
-  );
-};
+    <div className="IssueItem-body">{issue.body}</div>
+    <IssueComments issueNumber={issue.number} comments={issue.comments} />
+  </div>
+);
 
 export default IssueItem;
